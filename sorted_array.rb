f1c796d723bd55bb7f7a3031b63fa83cf2f5d2bf
darrenwong916 <darrenwong06@gmail.com>
2014-01-13 23:33:15 -0800

    Working #[]

--- sorted_array.rb
+++ sorted_array.rb
@@ -3,12 +3,14 @@ class SortedArray
 
   def initialize(input_arr = [])
     @internal_arr = []
-    input_arr.each {|split| add(split)}
+    input_arr.each {|new_ele| add(new_ele)}
+
   end
 
   def add(new_ele)
-    @internal_arr.insert(first_larger_index(new_ele), new_ele)
-    raise NotImplementedError.new("You need to implement the add method!")
+    # @internal_arr.insert(first_larger_index(new_ele), new_ele)
+     @internal_arr.insert(first_larger_index(new_elem, 0, @internal_arr.size), new_elem)
+
   end
 
   def size
@@ -16,17 +18,34 @@ class SortedArray
   end
 
   def [](index)
-    raise NotImplementedError.new("You need to implement the [] method!")
+    return @internal_arr[index]
+  
+     # return first_larger_index(index, start_ind=0, end_ind=@internal_arr.size)
+    # adding_index = @internal_arr.index {|x| x(index)}
+
   end
 
   def first_larger_index(target, start_ind=0, end_ind=@internal_arr.size)
-  
+
   # i = start_ind
   #   @internal_arr.slice(start_ind..end_ind).each { |x| 
   #     i += 1 if target > x
   #   }
   #   return i
   # end
+
+
+  #   array = @internal_arr.slice(start_ind..end_ind)
+  #   i = start_ind
+
+  #   array.each do |x|
+  #     i += 1 if target > x
+  #   end
+
+  #   i
+  # end
+
+
 
     mid_index = (start_ind + end_ind)/2
     if start_ind >= end_ind
@@ -40,7 +59,13 @@ class SortedArray
     end
   end
 
+
   def index(target)
-    raise NotImplementedError.new("You need to implement the index method!")
-  end
+     return_index = first_larger_index(target)
+    if @internal_arr[return_index] == target
+      return_index
+    else
+      target = nil
+    end
+end
 end
